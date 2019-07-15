88a7f3f6813024c5d2817f1f2665d8ff4fb11765
theonesp <ma.armengol.hcsc@gmail.com>
2019-07-15 12:13:50 -0400

    Update baseline lab results.sql
    sql style updated

--- baseline lab results.sql
+++ baseline lab results.sql
@@ -1,19 +1,64 @@
--- code for the baseline laboratory result
-
-select 
-co.icustay_id
-, avg(case when itemid in (51300,51301) then valuenum else null end) as avgwbc_baseline
-, avg(case when itemid = 51265 then valuenum else null end ) as avgplatelets_baseline
-, avg (case when itemid in (50983,50824) then valuenum else null end) as avgsodium_baseline
-, avg (case when itemid in (50971,50822) then valuenum else null end) as avgpotassium_baseline
-, avg (case when itemid = 51006 then valuenum else null end) as avgbun_baseline
-, avg (case when itemid in (50912,51081) then valuenum else null end) as avgcreatinine_baseline
-, avg (case when itemid in (50882,50803) then valuenum else null end) as avgbic_baseline
-from final_2 co -- replace by the table name created by you in the cohort selection
-inner join admissions adm
-on co.hadm_id = adm.hadm_id
-left join labevents le
-on co.subject_id = le.subject_id
-and le.charttime between adm.admittime - interval  '365' day and adm.admittime - interval '3' day
-group by co.subject_id, co.hadm_id, co.icustay_id, adm.admittime
-order by icustay_id
+  -- code for the baseline laboratory result
+SELECT
+  co.icustay_id,
+  AVG(CASE
+      WHEN itemid IN (51300, 51301) THEN valuenum
+    ELSE
+    NULL
+  END
+    ) AS avgwbc_baseline,
+  AVG(CASE
+      WHEN itemid = 51265 THEN valuenum
+    ELSE
+    NULL
+  END
+    ) AS avgplatelets_baseline,
+  avg (CASE
+      WHEN itemid IN (50983, 50824) THEN valuenum
+    ELSE
+    NULL
+  END
+    ) AS avgsodium_baseline,
+  avg (CASE
+      WHEN itemid IN (50971, 50822) THEN valuenum
+    ELSE
+    NULL
+  END
+    ) AS avgpotassium_baseline,
+  avg (CASE
+      WHEN itemid = 51006 THEN valuenum
+    ELSE
+    NULL
+  END
+    ) AS avgbun_baseline,
+  avg (CASE
+      WHEN itemid IN (50912, 51081) THEN valuenum
+    ELSE
+    NULL
+  END
+    ) AS avgcreatinine_baseline,
+  avg (CASE
+      WHEN itemid IN (50882, 50803) THEN valuenum
+    ELSE
+    NULL
+  END
+    ) AS avgbic_baseline
+FROM
+  final_2 co -- replace by the table name created by you in the cohort selection
+INNER JOIN
+  admissions adm
+ON
+  co.hadm_id = adm.hadm_id
+LEFT JOIN
+  labevents le
+ON
+  co.subject_id = le.subject_id
+  AND le.charttime BETWEEN adm.admittime - INTERVAL '365' day
+  AND adm.admittime - INTERVAL '3' day
+GROUP BY
+  co.subject_id,
+  co.hadm_id,
+  co.icustay_id,
+  adm.admittime
+ORDER BY
+  icustay_id
