79007bdabf17ea556aeab9ceab6d5d75c586e651
theonesp <ma.armengol.hcsc@gmail.com>
2019-07-15 12:10:30 -0400

    Update Primary ICD9 diagnosis.sql
    sql style updated

--- Primary ICD9 diagnosis.sql
+++ Primary ICD9 diagnosis.sql
@@ -1,7 +1,14 @@
--- Code for the primary ICD-9 diagnosis
-
-select final_2.hadm_id, primary_dx.icd9_code, primary_dx.short_title, primary_dx.long_title
-from public.final_2 -- replace by the table name created by you in the cohort selection
-left join public.primary_dx
-on public.final_2.hadm_id = public.primary_dx.hadm_id
-order by hadm_id
+  -- Code for the primary ICD-9 diagnosis
+SELECT
+  final_2.hadm_id,
+  primary_dx.icd9_code,
+  primary_dx.short_title,
+  primary_dx.long_title
+FROM
+  public.final_2 -- replace by the table name created by you in the cohort selection
+LEFT JOIN
+  public.primary_dx
+ON
+  public.final_2.hadm_id = public.primary_dx.hadm_id
+ORDER BY
+  hadm_id
