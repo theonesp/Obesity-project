56a73163b2e9c2807b422a961c47f3dc469fbbb4
theonesp <ma.armengol.hcsc@gmail.com>
2019-07-15 12:15:07 -0400

    Update 1st24hICU_labresult.sql
    sql style updated

--- 1st24hICU_labresult.sql
+++ 1st24hICU_labresult.sql
@@ -1,7 +1,18 @@
--- code for the most abnormal lab result in the first 24h of the ICU admission
-
-select final_2.icustay_id, labsfirstday.wbc_max as wbc_icu, labsfirstday.platelet_min as platelet_icu, labsfirstday.sodium_min as Na_icu, labsfirstday.potassium_max as k_icu, labsfirstday.bun_max as BUN_icu, labsfirstday.creatinine_max as Cr_icu, labsfirstday.bicarbonate_min as BIC_icu
-from public.final_2 -- replace by the table name created by you in the cohort selection
-left join labsfirstday
-on final_2.icustay_id = labsfirstday.icustay_id
-order by icustay_id
+  -- code for the most abnormal lab result in the first 24h of the ICU admission
+SELECT
+  final_2.icustay_id,
+  labsfirstday.wbc_max AS wbc_icu,
+  labsfirstday.platelet_min AS platelet_icu,
+  labsfirstday.sodium_min AS Na_icu,
+  labsfirstday.potassium_max AS k_icu,
+  labsfirstday.bun_max AS BUN_icu,
+  labsfirstday.creatinine_max AS Cr_icu,
+  labsfirstday.bicarbonate_min AS BIC_icu
+FROM
+  public.final_2 -- replace by the table name created by you in the cohort selection
+LEFT JOIN
+  labsfirstday
+ON
+  final_2.icustay_id = labsfirstday.icustay_id
+ORDER BY
+  icustay_id
