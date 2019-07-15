53ccff56b8b85237ff5601ae3ea068e400d49e64
theonesp <ma.armengol.hcsc@gmail.com>
2019-07-15 12:12:53 -0400

    Update ICU severity of illness score.sql
    sql style updated

--- ICU severity of illness score.sql
+++ ICU severity of illness score.sql
@@ -1,17 +1,32 @@
--- code for SAPS-II
-
-select final_2.icustay_id, sapsii.sapsii
-from public.final_2 -- replace by the table name created by you in the cohort selection
-left join sapsii
-on final_2.icustay_id = sapsii.icustay_id
-order by icustay_id
-
-
---code for SOFA
-
-select final_2.icustay_id, sofa.sofa as sofa_score, sofa.respiration, sofa.coagulation, sofa.liver, sofa.cardiovascular, sofa.cns,sofa.renal
-from public.final_2 -- replace by the table name created by you in the cohort selection
-left join sapsii
-left join sofa
-on final_2.icustay_id = sofa.icustay_id
-order by icustay_id
+  -- code for SAPS-II
+SELECT
+  final_2.icustay_id,
+  sapsii.sapsii
+FROM
+  public.final_2 -- replace by the table name created by you in the cohort selection
+LEFT JOIN
+  sapsii
+ON
+  final_2.icustay_id = sapsii.icustay_id
+ORDER BY
+  icustay_id
+  --code for SOFA
+SELECT
+  final_2.icustay_id,
+  sofa.sofa AS sofa_score,
+  sofa.respiration,
+  sofa.coagulation,
+  sofa.liver,
+  sofa.cardiovascular,
+  sofa.cns,
+  sofa.renal
+FROM
+  public.final_2 -- replace by the table name created by you in the cohort selection
+LEFT JOIN
+  sapsii
+LEFT JOIN
+  sofa
+ON
+  final_2.icustay_id = sofa.icustay_id
+ORDER BY
+  icustay_id
